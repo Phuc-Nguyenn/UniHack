b1460ee5c4a5167b69de0edfdd447555c621a3e8
JoeBannouna <yo.m.hsm3@gmail.com>
2024-03-03 17:11:46 +1030

    added routes for all other monkey actions

--- project/HTTPServerSetup.gd
+++ project/HTTPServerSetup.gd
@@ -3,6 +3,15 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var server = HttpServer.new()
+	
+	server.register_router("/start_jumping", MyExampleRouter.new())
+	server.register_router("/stop_jumping", MyExampleRouter.new())
+	
 	server.register_router("/start_ducking", MyExampleRouter.new())
+	server.register_router("/stop_ducking", MyExampleRouter.new())
+	
+	server.register_router("/start_swinging", MyExampleRouter.new())
+	server.register_router("/stop_swinging", MyExampleRouter.new())
+	
 	add_child(server)
 	server.start()
